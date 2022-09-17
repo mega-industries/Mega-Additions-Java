e6c41a5d37efa7fab8efbad02bee00f08f98bb3e
Drifted Notes <109656172+drfn@users.noreply.github.com>
2022-09-17 19:06:38 +0800

    Update SpikeBulletType.java

--- src/mega_adds/entities/bullet/SpikeBulletType.java
+++ src/mega_adds/entities/bullet/SpikeBulletType.java
@@ -9,15 +9,15 @@ public class SpikeBulletType extends BasicBulletType {
 
     public SpikeBulletType(float speed, float damage, Color color) {
         super(speed, damage, bulletSprite);
-        homingPower = 0.08 f;
-        shrinkY = 0 f;
-        width = 8 f;
-        height = 8 f;
+        homingPower = 0.08f;
+        shrinkY = 0;
+        width = 8;
+        height = 8;
         hitSound = Sounds.spark;
-        trailInterval = 0.4;
-        lifetime = 52 f;
+        trailInterval = 0.4f;
+        lifetime = 52;
         trailRotation = true;
-        trailEffect = new Effect(30 f, e - > {
+        trailEffect = new Effect(30f, e - > {
             Draw.color(fromColor, toColor, e.fin());
             Drawf.tri(e.x, e.y, width * e.fout(), height * e.fout(), e.rotation);
         }),
@@ -35,6 +35,6 @@ public class SpikeBulletType extends BasicBulletType {
     };
 
     public SpikeBulletType() {
-        this(1 f, 1 f, "bullet");
+        this(1f, 1f, "bullet");
     };
 }
