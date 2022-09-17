a5810848a9ad65b789dbfb6dc222131613aa455b
Drifted Notes <109656172+drfn@users.noreply.github.com>
2022-09-17 19:10:59 +0800

    Update SpikeBulletType.java

--- src/mega_adds/entities/bullet/SpikeBulletType.java
+++ src/mega_adds/entities/bullet/SpikeBulletType.java
@@ -6,7 +6,10 @@ import mindustry.graphics.*;
 public class SpikeBulletType extends BasicBulletType {
     public Color fromColor = Color.white.cpy();
     public Color toColor = Color.valueOf("f3e979");
-
+    public static final Effect default = new Effect(30f, e -> {
+        Draw.color(fromColor, toColor, e.fin());
+        Drawf.tri(e.x, e.y, width * e.fout(), height * e.fout(), e.rotation);
+    });
     public SpikeBulletType(float speed, float damage, Color color) {
         super(speed, damage, bulletSprite);
         homingPower = 0.08f;
@@ -17,10 +20,7 @@ public class SpikeBulletType extends BasicBulletType {
         trailInterval = 0.4f;
         lifetime = 52;
         trailRotation = true;
-        trailEffect = new Effect(30f, e -> {
-            Draw.color(fromColor, toColor, e.fin());
-            Drawf.tri(e.x, e.y, width * e.fout(), height * e.fout(), e.rotation);
-        }),
+        trailEffect = default;
     }
     
     public SpikeBulletType(float speed, float damage) {
